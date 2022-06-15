a3835d885b56e59413ea2ca8a2e9001e357c91f5
Atharva7K <atharva7kulkarni@gmail.com>
2022-06-15 11:52:29 +0530

    Login and Register working

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 from config import Config
 from flask_socketio import SocketIO
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+
 print(f' From __init__ {id(db)}')
 login = LoginManager(app)
 socketio = SocketIO(app)
-
-
 
 
 from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 from flask import render_template, flash, redirect, request, url_for
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
+from app import app
 from app.models import User
 from app import db
-from app import app
-from app.forms import LoginForm
+db.create_all()
+from app.forms import LoginForm, RegistrationForm
 from app import socketio
 from googletrans import Translator
 
@@ -12,13 +13,13 @@ print(f' From routes {id(db)}')
 sid2lang = {}
 translator = Translator()
 
-def broadcast(sender_sid, msg, sid2lang, translator, socket):
+def broadcast(username, sender_sid, msg, sid2lang, translator, socket):
     src = sid2lang[sender_sid]
     for client, lang in sid2lang.items():
         dest = lang
         if client != sender_sid:
             translated = translator.translate(msg, src=src, dest=dest).text
-            socket.emit('recieve', data=(translated, client), room=client)
+            socket.emit('recieve', data=(translated, username), room=client)
 
 @socketio.on('client_disconnected')
 def client_disconnected():
@@ -33,8 +34,10 @@ def client_connected(data):
 
 @socketio.on('send')
 def event(data):
-    print(f'from send. {request.sid}')
-    broadcast(request.sid, data['msg'], sid2lang, translator, socketio)
+    print(f'from send. {current_user.username}')
+    broadcast(current_user.username, request.sid, data['msg'], sid2lang, translator, socketio)
+    #broadcast(current_user.username, data['msg'], sid2lang, translator, socketio)
+
     #socketio.emit('recieve', data['msg'], broadcast=True)
 
 @app.route('/')
@@ -42,21 +45,46 @@ def event(data):
 def home():
     return render_template('index.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        print('legit user')
+        return redirect(url_for('lang'))
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         print('legit user')
         return redirect(url_for('lang'))
+    print('Not logged in')
     form = LoginForm()
     if form.validate_on_submit():
+        print('validated form')
         user = User.query.filter_by(username=form.username.data).first()
+        print(user)
         if user is None or not user.check_password(form.password.data):
+            print(' ran')
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('lang'))
     return render_template('login.html', form=form)
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 @app.route('/lang', methods=['GET', 'POST'])
 def lang():
